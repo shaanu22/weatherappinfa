10e3d0d2baaa328b4750a60a28fec77ecef1e8d7
shaanu22 <mails2samtaiwo@gmail.com>
2023-05-13 03:57:40 -0500

    Modified main.tf file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+
+variable "vpc-cidr" {}
+
+variable "subnet-cidr" {}
+
+variable "my_ip_cidr" {}
+
+variable "ubuntu_ami" {}
+
+variable "redhat_ami" {}
+
+variable "instance-keyname" {}
+
+variable "instance-type" {}
